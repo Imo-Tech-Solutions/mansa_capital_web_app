245e709bd604e4e6c7fb4cee881a123dc3805a96
samuelmoiwa <samuelmoiwa@gmail.com>
2023-10-10 12:38:29 +0000

    update FindYourBank.jsx

--- src/pages/FindYourBank.jsx
+++ src/pages/FindYourBank.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/findYourBank/Banner'
+import SearchBank from '../components/findYourBank/SearchBank';
 
 const FindYourBank = () => {
   return (
     <div>
         <Banner />
+        <SearchBank/>
     </div>
   )
 }
