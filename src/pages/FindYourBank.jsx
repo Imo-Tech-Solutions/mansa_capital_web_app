2c695e5aaabd2c8ab3de24653e928401d96bff55
samuelmoiwa <samuelmoiwa@gmail.com>
2023-10-10 15:32:59 +0000

    update FindYourBank.jsx

--- src/pages/FindYourBank.jsx
+++ src/pages/FindYourBank.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Banner from '../components/findYourBank/Banner'
 import SearchBank from '../components/findYourBank/SearchBank';
+import SpendingAccount from '../components/findYourBank/SpendingAccount';
+import SavingsAccount from '../components/findYourBank/SavingsAccount';
+import PremiumAccount from '../components/findYourBank/PremiumAccount';
+import ElevateMembership from '../components/findYourBank/ElevateMembership';
 
 const FindYourBank = () => {
   return (
     <div>
         <Banner />
         <SearchBank/>
+        <SpendingAccount/>
+        <SavingsAccount />
+        <PremiumAccount />
+        <ElevateMembership />
     </div>
   )
 }
