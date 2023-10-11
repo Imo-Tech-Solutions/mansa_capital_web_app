002ed640740fc72cd6823b6dad37d25038c6e62e
samuelmoiwa <samuelmoiwa@gmail.com>
2023-10-11 10:29:03 +0000

    update Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -10,14 +10,14 @@ const Services = () => {
       <Banner />
       <KeyServiceAreas />
       <div className='w-full flex justify-center mb-24 mt-16'>
-        <div className='flex flex-row items-center justify-between w-10/12 h-60 rounded-xl bg-orange-500
+        <div className='flex sm:flex-row flex-col items-center justify-between w-10/12 sm:h-60 py-4 rounded-xl bg-orange-500
           bg-center bg-no-repeat px-10
         '
         style={{ backgroundImage: `url(${dorts})` }}
         >
           <div className='flex flex-col justify-center  h-full'>
-            <h2 className='text-white font-bold text-6xl mb-5'>Get a free consultation</h2>
-            <p className='text-white text-3xl'>Will Help You Solve Accounting and Taxation Issues</p>
+            <h2 className='text-white font-bold sm:text-6xl text-5xl mb-5'>Get a free consultation</h2>
+            <p className='text-white sm:text-3xl text-xl'>Will Help You Solve Accounting and Taxation Issues</p>
           </div>
 
           <Link
